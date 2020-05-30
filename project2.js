33dec871aa986fb9d09595b1dd2c864d893943e2
tsagaantsooj-design <61138473+tsagaantsooj-design@users.noreply.github.com>
2020-05-29 22:25:28 -0700

    Delete project2.js

--- project2.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-fs.writeFile("cs142-make-multi-filter.js", "Hey there!", function(err) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("The file was saved!");
-    }
-}); 
